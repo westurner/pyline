53ad0f4d2b6015928884a69e26d413c25d02be84
Wes Turner <wes@wrd.nu>
2014-05-12 20:30:47 -0500

    DOC: version bump, setup description

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def build_long_description():
 
 setup(
     name='pyline',
-    version='0.1.2',
+    version='0.1.3',
     description=(
         'A grep-like, sed-like command-line tool for line-based '
         'processing in Python.'),
