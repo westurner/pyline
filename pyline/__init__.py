9866e49e33e10e8df45971c809126804ad4c1cd0
Wes Turner <wes@wrd.nu>
2016-07-22 11:57:47 -0500

    BUG: pyline/__init__.py: relative imports for python3 (#7)

--- pyline/__init__.py
+++ pyline/__init__.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*-
 
 
-import pyline
+import sys
+
+if sys.version_info.major > 2:
+    import pyline.pyline as pyline
+else:
+    import pyline
+
 __version__ = version = pyline.version
 
 # __main__ = pyline.main_entrypoint
